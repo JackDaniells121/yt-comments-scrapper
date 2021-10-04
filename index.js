8365db5dcffa044a5eb118eeb4b2374c1314f978
JackDaniells121 <jakub.ujwary@gmail.com>
2021-10-04 20:53:46 +0200

    first working authors

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const puppeteer = require('puppeteer');
 
+const firefoxOptions = {
+    product: 'firefox',
+    extraPrefsFirefox: {
+      // Enable additional Firefox logging from its protocol implementation
+      // 'remote.log.level': 'Trace',
+    },
+    // Make browser logs visible
+    dumpio: true,
+  };
+
+const video_id = 'F2LOrLh77F0';
+
 (async () =>{
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch(firefoxOptions);
     const page = await browser.newPage();
-    await page.goto('https://example/com');
-    await page.screenshot({path:'example.png'});
+    page.setViewport({ width:1920, height: 1080});
+    await page.goto('https://www.youtube.com/live_chat?v='+video_id);
+    await page.screenshot({path:'example2.png'});
+    const messageNodes = await page.$$('yt-live-chat-text-message-renderer');
+    
+    const texts = await Promise.all(
+        messageNodes.map(
+            message => message.$eval("#author-name", el => el.innerText)
+        )
+    ); 
+        
+    console.log(texts);
 
     await browser.close();
 })();
